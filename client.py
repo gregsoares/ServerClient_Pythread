d3686dacf29de3e003c5cbdb066bd6ab89f9b6b6
gregsoares <sfag1988>
2018-02-12 02:40:14 -0500

    Authentication Working
    Bug: second message doesn't send properly
    Something wrong with recognizing exit_code on client side

--- client.py
+++ client.py
@@ -1,116 +1,63 @@
 #Author: Greg Soares
 #!/usr/bin/env python
 
-
-import time
-import timeit
 import socket
-import threading
-#import Queue
-      
-L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
-I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
 
-key = 7
-
-#class Low_Level_encrypt():
-#    def __init__(self):
-#        return encrypt(self.plaintext)
-#
-#    def encrypt(ptext):
-#        binVar_i = ''
-#        plaintext = ""
-#        plaintext = ptext
-#
-#        # encipher
-#        ciphertext = ""
-#        for c in plaintext.upper():
-#            if c.isalpha(): ciphertext += I2L[ (L2I[c] + key)%26 ]
-#            else: ciphertext += c
-#
-#        # decipher
-#        plaintext2 = ""
-#        for c in ciphertext.upper():
-#            if c.isalpha(): plaintext2 += I2L[ (L2I[c] - key)%26 ]
-#            else: plaintext2 += c
-#        print ('Decrypted Ciphertext:')
-#        print (plaintext2)
-#        ciphertext = list(ciphertext) #Turn string to array of chars
-#        #print ciphertext
-#        binary_ciphertext = []
-#
-#        for charToBin in ciphertext: #Char -> Int -> bin: then it to binary_ciphertext and return
-#            binVar_i = ord(charToBin) #take the char in the string and turn it to integer
-#            binVar = bin(binVar_i) #from integer to binary
-#            binVar_i = list(str(binVar))
-#            print (binVar_i)
-#            if len(binVar_i) <= 8:
-#                print ('THIS IS LESS THAN 7 ---  ')
-#                print (binVar_i)
-#                while len(binVar_i) < 9:
-#                    binVar_i.insert(0,0)
-#                    print ('\n -- ADDED 0 --')
-#                    print (binVar_i)
-#            for each in binVar_i: #Takes each binary location and checks that its a number, then adds to the message to be
-#                #sent
-#                if each == '0' or each == '1':
-#                    binary_ciphertext.append(each)
-#        print ("\n---ENCRYPTED CIPHER ---\n")
-#        print (ciphertext)
-#        #print "\n-----------------------"
-#        #print ('TESTING binary_ciphertext\n -----------------\n')
-#        #print (binary_ciphertext)
-#        return (binary_ciphertext)
-#        
-#
 def Main():
-    encoded = ""
-    # encoded = encrypt(plaintext)
-    #print '\nCiphertext to be Sent:'
-    #for eachElement in message: print eachElement
-
-    #print '\nPrinting each letter as array of chars(casted to byte)'
-    #print encoded
-
-# Initiating Socket
-
-#----Initiate a loop to encode each char in string and send it    
-    #message = raw_input("-> ")
-
-    # i = 0
-    # while i != len(encoded):
-    #    message = encoded[i]
-    #    #print('Sending NOW: %s'%(message))
-    #    s.send('%s'%(message))
-    #    #data = s.recv(1024)
-    #    #print 'Received from server: ' + str(data)
-    # ser.write('\nTransmitted Ciphertext Message: \n%s'%(encoded))
-    #    i = i + 1
-    #    time.sleep(.1)
-
+    exit_code = '!@'
     host = '127.0.0.1'
     port = 5000
-    login = input("Login: ")
-    message = input("Message: ")
     s = socket.socket()
     s.connect((host, port))
+
+    # Initiating connection
+    login = input("Login: ")
+    message = input("Message: ")
+    login = (login.encode())
+
     print("Sending Message")
-    s.send((login.encode()))
+    s.send(login)
     data = s.recv(10)
-    if(data == (login.encode())):
-        s.send((message.encode()))
+    auth_recv = (str(data))
+    login.decode()
+    login = (str(login))
+    # Debugging messages
+    # print('Checking Login')
+    # print(auth_recv + ' : ' + login)
+    # Add a timer to the if statement to disconnect if no auth received
+    if auth_recv == login:  # If we get out auth back then send message
+        print('Sending message')  # Let client know we got auth and message is being sent
+        message = (message.encode())
+        s.send(message)
+        # message = message.decode()
+        # message = (str(message))
         keep_alive = True
         data = s.recv(1024)
-        if(data == ( message.encode())):
+        data = (str(data))
+        message = (str(message))
+        print('Checking: ' + data + '= ' + message)
+        if data == message:
+
+            print(exit_code)
             while keep_alive:
-                new_message = input("Send: ")
+                data = s.recv(1024)
+                print(data)
+                new_message = (input("Send: ")).encode()
                 s.send(new_message)
                 data = s.recv(1024)
-                if (data != new_message):
-                    print('\t\t-- Message error -- \n Sent: {} \n Received: {} '.format((str(new_message)), (str(data))))
+                data = (str(data))
+                new_message = (str(new_message))
+                # Debugging messages
+                # print(data)
+                # print(new_message)
+                if new_message == exit_code:  # Check for exit code and close connection
+                    print('DISCONNECTING')
                     keep_alive = False
-                else:
-                    pass
+
+#                if data != new_message:
+#                    print('\t\t-- Message error -- \n Sent: {} \n Received: {} '.format(new_message, data))
+#                    keep_alive = False
+
     s.close()
 
 
--- server.py
+++ server.py
@@ -1,111 +1,15 @@
-# Author: Greg Soares
-# Server Class and threading from github.com/Torxed
 #!/usr/bin/env python
 
 
-import time
-import timeit
 import socket
 import threading
 
-#import Queue
-
-# {
-#
-# L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
-# I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
-# key = 7
-#class Low_Level_encrypt():
-#    def __init__(self):
-#        return encrypt(self.plaintext)
-#
-#    def encrypt(ptext):
-#        binVar_i = ''
-#        plaintext = ""
-#        plaintext = ptext
-#
-#        # encipher
-#        ciphertext = ""
-#        for c in plaintext.upper():
-#            if c.isalpha(): ciphertext += I2L[ (L2I[c] + key)%26 ]
-#            else: ciphertext += c
-#
-#        # decipher
-#        plaintext2 = ""
-#        for c in ciphertext.upper():
-#            if c.isalpha(): plaintext2 += I2L[ (L2I[c] - key)%26 ]
-#            else: plaintext2 += c
-#        print ('Decrypted Ciphertext:')
-#        print (plaintext2)
-#        ciphertext = list(ciphertext) #Turn string to array of chars
-#        #print ciphertext
-#        binary_ciphertext = []
-#
-#        for charToBin in ciphertext: #Char -> Int -> bin: then it to binary_ciphertext and return
-#            binVar_i = ord(charToBin) #take the char in the string and turn it to integer
-#            binVar = bin(binVar_i) #from integer to binary
-#            binVar_i = list(str(binVar))
-#            print (binVar_i)
-#            if len(binVar_i) <= 8:
-#                print ('THIS IS LESS THAN 7 ---  ')
-#                print (binVar_i)
-#                while len(binVar_i) < 9:
-#                    binVar_i.insert(0,0)
-#                    print ('\n -- ADDED 0 --')
-#                    print (binVar_i)
-#            for each in binVar_i: #Takes each binary location and checks that its a number, then adds to the message to be
-#                #sent
-#                if each == '0' or each == '1':
-#                    binary_ciphertext.append(each)
-#        print ("\n---ENCRYPTED CIPHER ---\n")
-#        print (ciphertext)
-#        #print "\n-----------------------"
-#        #print ('TESTING binary_ciphertext\n -----------------\n')
-#        #print (binary_ciphertext)
-#        return (binary_ciphertext)
-#        
-#
-#def Main():
-#    host = '172.16.201.109'
-#    port = 2041 
-#    message = ""
-#    encoded = ""
-#    plaintext = "Twinkle, twinkle, little star\nHow I wonder what you are\nUp above the world so high\nLike a diamond in the sky\nTwinkle, twinkle little star\nHow I wonder what you are"
-#    plaintext = plaintext.upper()
-#    # encoded = encrypt(plaintext)
-#    #print '\nCiphertext to be Sent:'
-#    #for eachElement in message: print eachElement
-#
-#    #print '\nPrinting each letter as array of chars(casted to byte)'
-#    #print encoded
-#
-## Initiating Socket
-#    s = socket.socket()
-#    s.connect((host, port))
-##----Initiate a loop to encode each char in string and send it    
-#    #message = raw_input("-> ")
-#
-#    i = 0
-#    #while i != len(encoded):
-#    #    message = encoded[i]
-#    #    #print('Sending NOW: %s'%(message))
-#    #    s.send('%s'%(message))
-#    #    #data = s.recv(1024)
-#    #    #print 'Received from server: ' + str(data)
-#    ##ser.write('\nTransmitted Ciphertext Message: \n%s'%(encoded))
-#    #    i = i + 1
-#    #    time.sleep(.1)
-#    print("Sending Message")
-#    messa = 'asd'
-#    s.send('a')
-#    data = s.recv(1024)
-#    s.close()
-#
-# }
 def Main():
-    auth = "pass"
+    auth = 'pass'
     host = '127.0.0.1'
     port = 5000
+    exit_code = '!@'
+    exit_code = exit_code.encode()
     s = socket.socket()
     s.bind((host, port))
     s.listen(0)
@@ -113,23 +17,26 @@ def Main():
     print("Incoming connection from: " + str(addr))
 
     data = c.recv(10)  # Receive max of 10 bytes
-    auth_recv = str(data)
-    auth_recv = auth_recv.decode()
+    auth_recv = str((data.decode()))
+#    auth_recv = auth_recv.decode()
     print('Checking: ' + auth_recv)
+    print(auth + ' is of type ' + str(type(auth)))
+    print(auth_recv + ' is of type ' + str(type(auth_recv)))
 
-    if (auth_recv == auth):
-        print(auth_recv + ' = greg')
-    while data != ('!@'.encode()):  # Exit code
-        # Make an is_authorized to check for login again or not
-        # ELSE: if is_authorized then just receive message till exit code is given
-        c.send(data)  # ack
-        data = c.recv(1024)  # Receive max of 10 bytes
-        c.send(data)  # ack
-        print ("Message: " + data.decode('ascii'))
-        if not data:
-            break
-
-
+    if ((str(auth_recv)) == (str(auth))):
+        print(auth_recv + ' successfully authenticated')
+        print('Exit Code: ' + (str(exit_code)))
+        while data != exit_code:  # Exit code
+            # Make an is_authorized to check for login again or not
+            # ELSE: if is_authorized then just receive message till exit code is given
+            c.send(data)  # ack
+            data = c.recv(1024)  # Receive max of 10 bytes
+            c.send(data)  # ack
+            print("Message: " + data.decode('ascii'))
+            if not data:
+                break
+    else:
+        print(auth_recv + ' did not authenticate successfully')
 
     c.close()
 
